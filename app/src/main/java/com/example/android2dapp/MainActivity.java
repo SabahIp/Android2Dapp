ad25ea71f1f7a3b914f4f6a8470871a57f6cb082
SabahIp <sabah@rhrk.uni-kl.de>
2023-11-21 17:04:27 +0100

    i learn

--- app/src/main/java/com/example/android2dapp/MainActivity.java
+++ app/src/main/java/com/example/android2dapp/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.android2dapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //Vollerscreen und ich mag keine Bar haben
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        Window window =getWindow();
+        window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
+        // set content view to game so that objects in the Game class can be rendered to the screen
+        setContentView(new Game(this));
     }
 }
